a1a80d758c2e7ce7b278ac0fcf14a7209f0d8808
kangdaiyu <kangdaiyu88@gmail.com>
2023-09-25 18:07:02 +0700

    refactor: return market info

--- hmx2/modules/public.py
+++ hmx2/modules/public.py
@@ -1,6 +1,94 @@
+from web3 import Web3
+from hmx2.constants import (
+  TOKEN_PROFILE,
+  COLLATERAL_WETH,
+    MULTICALL_ADDRESS,
+    CROSS_MARGIN_HANDLER_ADDRESS,
+    LIMIT_TRADE_HANDLER_ADDRESS,
+    VAULT_STORAGE_ADDRESS,
+    PERP_STORAGE_ADDRESS,
+    CONFIG_STORAGE_ADDRESS,
+    CROSS_MARGIN_HANDLER_ABI_PATH,
+    LIMIT_TRADE_HANDLER_ABI_PATH,
+    VAULT_STORAGE_ABI_PATH,
+    PERP_STORAGE_ABI_PATH,
+    CONFIG_STORAGE_ABI_PATH,
+    ERC20_ABI_PATH,
+    COLLATERALS,
+    COLLATERAL_ASSET_ID_MAP,
+    ADDRESS_ZERO,
+    MAX_UINT,
+    EXECUTION_FEE,
+    BPS,
+    HOURS,
+    DAYS,
+    YEARS,
+    MARKET_PROFILE
+)
+from hmx2.helpers.contract_loader import load_contract
+from hmx2.modules.oracle.oracle_middleware import OracleMiddleware
+from hmx2.modules.calculator.fee import FeeCalculator
+from simple_multicall import Multicall
+from eth_abi.abi import encode, decode
+
+
 class Public(object):
-  def __init__(self, eth_provider):
+  def __init__(self, eth_provider: Web3, oracle_middleware: OracleMiddleware):
     self.eth_provider = eth_provider
+    self.oracle_middleware = oracle_middleware
+    self.perp_storage_instance = load_contract(
+      self.eth_provider, PERP_STORAGE_ADDRESS, PERP_STORAGE_ABI_PATH)
+    self.config_storage_instance = load_contract(
+      self.eth_provider, CONFIG_STORAGE_ADDRESS, CONFIG_STORAGE_ABI_PATH)
+    self.vault_storage_instance = load_contract(
+      self.eth_provider, VAULT_STORAGE_ADDRESS, VAULT_STORAGE_ABI_PATH)
+    self.multicall_instance = Multicall(w3=self.eth_provider,
+                                        chain='arbitrum', custom_address=MULTICALL_ADDRESS)
+
+  def get_trading_config(self):
+    (
+      funding_interval,
+      min_profit_duration,
+      dev_fee_rate_bps,
+      max_position
+    ) = self.config_storage_instance.functions.tradingConfig().call()
+    return {
+       "funding_interval": funding_interval,
+        "min_profit_duration": min_profit_duration,
+        "dev_fee_rate_bps": dev_fee_rate_bps,
+        "max_position": max_position,
+    }
+
+  def get_market_config(self, market_index: int):
+    (
+      asset_id,
+      max_long_position_size,
+      max_short_position_size,
+      increase_position_fee_rate_bps,
+      decrease_position_fee_rate_bps,
+      initial_margin_fraction_bps,
+      maintenance_margin_fraction_bps,
+      max_profit_rate_bps,
+      asset_class,
+      allow_increase_position,
+      active,
+      (max_skew_scale_usd, max_funding_rate)
+    ) = self.config_storage_instance.functions.marketConfigs(market_index).call()
+    return {
+      "asset_id": asset_id.hex(),
+      "max_long_position_size": max_long_position_size,
+      "max_short_position_size": max_short_position_size,
+      "increase_position_fee_rate_bps": increase_position_fee_rate_bps,
+      "decrease_position_fee_rate_bps": decrease_position_fee_rate_bps,
+      "initial_margin_fraction_bps": initial_margin_fraction_bps,
+      "maintenance_margin_fraction_bps": maintenance_margin_fraction_bps,
+      "max_profit_rate_bps": max_profit_rate_bps,
+      "asset_class": asset_class,
+      "allow_increase_position": allow_increase_position,
+      "active": active,
+      "max_skew_scale_usd": max_skew_scale_usd,
+      "max_funding_rate": max_funding_rate
+    }
 
   def get_market(self, market_index: int):
     (
@@ -28,4 +116,205 @@ class Public(object):
       "accum_funding_long": accum_funding_long,
       "accum_funding_short": accum_funding_short,
       "funding_accrued": funding_accrued,
+    }
+
+  def multicall_market_data(self, market_index: int):
+    calls = [
+      self.multicall_instance.create_call(
+        self.config_storage_instance, "marketConfigs", [market_index]
+      ),
+      self.multicall_instance.create_call(
+        self.perp_storage_instance, "markets", [market_index]
+      ),
+      self.multicall_instance.create_call(
+        self.config_storage_instance, "tradingConfig", []
+      )
+    ]
+
+    results = self.multicall_instance.call(calls)
+    data = results[1]
+    (
+      asset_id,
+      max_long_position_size,
+      max_short_position_size,
+      increase_position_fee_rate_bps,
+      decrease_position_fee_rate_bps,
+      initial_margin_fraction_bps,
+      maintenance_margin_fraction_bps,
+      max_profit_rate_bps,
+      asset_class,
+      allow_increase_position,
+      active,
+      max_skew_scale_usd,
+      max_funding_rate,
+    ) = decode(
+        ['bytes32', 'uint256', 'uint256', 'uint32', 'uint32', 'uint32',
+         'uint32', 'uint32', 'uint8', 'bool', 'bool', 'uint256', 'uint256'],
+        data.pop(0)
+      )
+    (
+      long_position_size,
+      long_accum_se,
+      long_accum_s2e,
+      short_position_size,
+      short_accum_se,
+      short_accum_s2e,
+      current_funding_rate,
+      last_funding_time,
+      accum_funding_long,
+      accum_funding_short,
+      funding_accrued,
+    ) = decode(
+      ['uint256', 'uint256', 'uint256', 'uint256', 'uint256',
+       'uint256', 'int256', 'uint256', 'int256', 'int256', 'int256'],
+      data.pop(0)
+    )
+    (
+      funding_interval,
+      min_profit_duration,
+      dev_fee_rate_bps,
+      max_position
+    ) = decode(
+      ['uint256', 'uint256', 'uint32', 'uint8'],
+      data.pop(0)
+    )
+
+    calls = [
+      self.multicall_instance.create_call(
+        self.config_storage_instance, "assetClassConfigs", [asset_class]
+      ),
+      self.multicall_instance.create_call(
+        self.perp_storage_instance, "assetClasses", [asset_class]
+      ),
+    ]
+    results = self.multicall_instance.call(calls)
+    data = results[1]
+    base_borrowing_rate = int(data.pop(0).hex(), 16)
+    (
+      reserve_value_e30,
+      sum_borrowing_rate,
+      last_borrowing_time,
+      sum_borrowing_fee_e30,
+      sum_settled_borrowing_fee_e30
+    ) = decode(
+      ['uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
+      data.pop(0)
+    )
+
+    return {
+      "market_config": {
+        "asset_id": asset_id.hex(),
+        "max_long_position_size": max_long_position_size,
+        "max_short_position_size": max_short_position_size,
+        "increase_position_fee_rate_bps": increase_position_fee_rate_bps,
+        "decrease_position_fee_rate_bps": decrease_position_fee_rate_bps,
+        "initial_margin_fraction_bps": initial_margin_fraction_bps,
+        "maintenance_margin_fraction_bps": maintenance_margin_fraction_bps,
+        "max_profit_rate_bps": max_profit_rate_bps,
+        "asset_class": asset_class,
+        "allow_increase_position": allow_increase_position,
+        "active": active,
+        "max_skew_scale_usd": max_skew_scale_usd,
+        "max_funding_rate": max_funding_rate
+      },
+      "market": {
+        "long_position_size": long_position_size,
+        "long_accum_se": long_accum_se,
+        "long_accum_s2e": long_accum_s2e,
+        "short_position_size": short_position_size,
+        "short_accum_se": short_accum_se,
+        "short_accum_s2e": short_accum_s2e,
+        "current_funding_rate": current_funding_rate,
+        "last_funding_time": last_funding_time,
+        "accum_funding_long": accum_funding_long,
+        "accum_funding_short": accum_funding_short,
+        "funding_accrued": funding_accrued
+      },
+      "trading_config": {
+        "funding_interval": funding_interval,
+        "min_profit_duration": min_profit_duration,
+        "dev_fee_rate_bps": dev_fee_rate_bps,
+        "max_position": max_position
+      },
+      "asset_class_config": {
+        "base_borrowing_rate": base_borrowing_rate
+      },
+      "asset_class": {
+        "reserve_value_e30": reserve_value_e30,
+        "sum_borrowing_rate": sum_borrowing_rate,
+        "last_borrowing_time": last_borrowing_time,
+        "sum_borrowing_fee_e30": sum_borrowing_fee_e30,
+        "sum_settled_borrowing_fee_e30": sum_settled_borrowing_fee_e30
+      }
+    }
+
+  def get_block(self):
+    return self.eth_provider.eth.get_block("latest")
+
+  def get_hlp_tvl(self):
+    calls = [
+      self.multicall_instance.create_call(
+        self.vault_storage_instance, "hlpLiquidity", [collateral])
+      for collateral in COLLATERALS
+    ]
+    collateral_usd = [
+      self.oracle_middleware.get_price(
+        COLLATERAL_ASSET_ID_MAP[collateral]) * 10**10
+      for collateral in COLLATERALS
+    ]
+    results = self.multicall_instance.call(calls)
+
+    return sum(
+      [
+        int(results[1][index].hex(), 16) *
+          collateral_usd[index] // 10 ** TOKEN_PROFILE[collateral]["decimals"]
+          for index, collateral in enumerate(COLLATERALS)
+      ]
+    )
+
+  def get_market_price(self, market_index: int):
+    return self.oracle_middleware.get_price(
+      MARKET_PROFILE[market_index]["asset"])
+
+  def get_market_info(self, market_index: int):
+    '''
+    Get a market info
+
+    :param market_index: requied
+    :type market_index: int in list Market
+    '''
+    data = self.multicall_market_data(market_index)
+    block = self.get_block()
+    tvl = self.get_hlp_tvl()
+
+    price = self.oracle_middleware.get_price(
+      MARKET_PROFILE[market_index]["asset"])
+
+    funding_rate = FeeCalculator.get_funding_rate(
+      data["trading_config"],
+      data["market_config"],
+      data["market"],
+      block["timestamp"],
+    )
+
+    borrowing_rate = FeeCalculator.get_borrowing_rate(
+      data["asset_class_config"], data["asset_class"], tvl)
+
+    return {
+      "market": MARKET_PROFILE[market_index]["name"],
+      "price": price,
+      "long_size": data["market"]["long_position_size"] / 10**30,
+      "short_size": data["market"]["short_position_size"] / 10**30,
+      "funding_rate": {
+        "1H": funding_rate * HOURS / DAYS * 100 / 10**18,
+        "8H": funding_rate * 8 * HOURS / DAYS * 100 / 10**18,
+        "24H": funding_rate * 100 / 10**18,
+        "1Y": funding_rate * 365 * 100 / 10**18,
+      },
+      "borrowing_rate": {
+        "1H": borrowing_rate * HOURS / 10**18,
+        "8H": borrowing_rate * 8 * HOURS / 10**18,
+        "24H": borrowing_rate * DAYS / 10**18,
+        "1Y": borrowing_rate * YEARS / 10**18,
+      }
     }

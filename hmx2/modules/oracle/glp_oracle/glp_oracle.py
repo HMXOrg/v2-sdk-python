d8b61c69eda3beb4067649fd79ececeda9cf18f7
chenciao8 <chenciao8@gmail.com>
2024-04-24 19:22:36 +0700

    feat: add user infos

--- hmx2/modules/oracle/glp_oracle/glp_oracle.py
+++ hmx2/modules/oracle/glp_oracle/glp_oracle.py
@@ -3,6 +3,8 @@ from hmx2.helpers.contract_loader import load_contract
 from hmx2.constants.contracts import GLP_MANAGER_ABI_PATH
 from hmx2.constants.assets import ASSET_GLP
 from hmx2.helpers.mapper import get_contract_address
+from simple_multicall_v6 import Multicall
+from eth_abi.abi import decode
 
 
 class GlpOracle(object):
@@ -11,6 +13,8 @@ class GlpOracle(object):
     self.contract_address = get_contract_address(self.eth_provider.eth.chain_id)
     self.glp_manager_instance = load_contract(
       self.eth_provider, self.contract_address["GLP_MANAGER_ADDRESS"], GLP_MANAGER_ABI_PATH)
+    self.multicall_instance = Multicall(w3=self.eth_provider,
+                                        custom_address=self.contract_address["MULTICALL_ADDRESS"])
 
   def get_price(self, asset_id: str) -> float:
     '''
@@ -22,7 +26,15 @@ class GlpOracle(object):
     if asset_id != ASSET_GLP:
       raise Exception('Only GLP is supported')
 
-    mid_price = (self.glp_manager_instance.functions.getPrice(False).call() +
-                 self.glp_manager_instance.functions.getPrice(True).call()) / 2
+    calls = [self.multicall_instance.create_call(
+      self.glp_manager_instance,
+      "getPrice",
+      [is_true]
+    ) for is_true in [False, True]]
+
+    contract_data_raw = self.multicall_instance.call(calls)[1]
+
+    mid_price = (decode(['uint256'], contract_data_raw[0])[0] +
+                 decode(['uint256'], contract_data_raw[1])[0]) / 2
 
     return mid_price / 10 ** 30
